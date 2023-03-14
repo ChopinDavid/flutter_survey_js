e86c9da48ecf0e24f62a9696b48a82399c233e0e
Dan Couturier <daniel.couturier@wwt.com>
2023-03-14 13:14:29 -0400

    77472 - removed survey styles configuration class - DJC/DC

--- lib/ui/survey_styles_configuration.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SurveyStylesConfiguration {
-  SurveyStylesConfiguration({this.questionTitleTextStyle});
-  TextStyle? questionTitleTextStyle;
-}
--- lib/ui/survey_widget.dart
+++ lib/ui/survey_widget.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_survey_js/generated/l10n.dart';
 import 'package:flutter_survey_js/model/survey.dart' as s;
-import 'package:flutter_survey_js/ui/survey_styles_configuration.dart';
 import 'package:im_stepper/stepper.dart';
 import 'package:logging/logging.dart';
 import 'package:reactive_forms/reactive_forms.dart';
@@ -21,7 +20,6 @@ class SurveyWidget extends StatefulWidget {
   final bool showQuestionsInOnePage;
   final SurveyController? controller;
   final bool hideSubmitButton;
-  final SurveyStylesConfiguration? stylesConfiguration;
 
   const SurveyWidget({
     Key? key,
@@ -32,7 +30,6 @@ class SurveyWidget extends StatefulWidget {
     this.showQuestionsInOnePage = false,
     this.controller,
     this.hideSubmitButton = false,
-    this.stylesConfiguration,
   }) : super(key: key);
   @override
   State<StatefulWidget> createState() => SurveyWidgetState();
@@ -166,7 +163,6 @@ class SurveyWidgetState extends State<SurveyWidget> {
         survey: widget.survey,
         formGroup: formGroup,
         elementsState: elementsState,
-        stylesConfiguration: widget.stylesConfiguration,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
@@ -281,13 +277,11 @@ class SurveyProvider extends InheritedWidget {
   final s.Survey survey;
   final FormGroup formGroup;
   final ElementsState elementsState;
-  final SurveyStylesConfiguration? stylesConfiguration;
   SurveyProvider({
     required this.elementsState,
     required this.child,
     required this.survey,
     required this.formGroup,
-    this.stylesConfiguration,
   }) : super(child: child);
 
   static SurveyProvider of(BuildContext context) {
