44e987918ba3c6ab875b654f8249cf04ffeb94de
Dan Couturier <daniel.couturier@wwt.com>
2023-03-14 14:37:31 -0400

    77472 - Using Context of Theme for Dropdown widget - DJC/DC

--- lib/ui/elements/survey_element_factory.dart
+++ lib/ui/elements/survey_element_factory.dart
@@ -94,9 +94,15 @@ class SurveyElementFactory {
       return ReactiveDropdownField(
         formControlName: element.name!,
         items: e.choices
-                ?.map((e) => DropdownMenuItem(
+                ?.map(
+                  (e) => DropdownMenuItem(
                     value: e.value,
-                    child: Text(e.text ?? e.value?.toString() ?? "")))
+                    child: Text(
+                      e.text ?? e.value?.toString() ?? '',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                  ),
+                )
                 .toList(growable: false) ??
             [],
       ).wrapQuestionTitle(element, hasTitle: hasTitle);
@@ -126,7 +132,10 @@ class SurveyElementFactory {
     //     ));
 
     unsupported = (context, element, {bool hasTitle = true}) => Container(
-          child: Text('Unsupported ${element.name ?? ""}'),
+          child: Text(
+            'Unsupported ${element.name ?? ""}',
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
         ).wrapQuestionTitle(element, hasTitle: hasTitle);
   }
 
