b173d93eecf5342851e36e008b2db9bdd073b97c
Wes Ehrlichman <wes.ehrlichman@wwt.com>
2023-03-15 14:31:04 -0500

    Make survey non-optional because it is always non-null at the time the examples are built - WE/DJC

--- example/lib/components/simple.dart
+++ example/lib/components/simple.dart
@@ -2,52 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter_survey_js/survey.dart' as s;
 
 class Simple extends StatelessWidget {
-  final s.Survey? survey;
+  final s.Survey survey;
 
-  Simple({Key? key, this.survey}) : super(key: key);
+  Simple({Key? key, required this.survey}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text('Survey test'),
-            ),
-            body: survey == null
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : s.SurveyWidget(
-                    survey: survey!,
-                    onChange: (v) {
-                      print(v);
-                    },
-                    onSubmit: (v) {
-                      print(v);
-                      showModalBottomSheet<void>(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return Container(
-                            height: 400,
-                            child: Center(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  Expanded(
-                                      child: Container(
-                                          child: SingleChildScrollView(
-                                              child: Text(v.toString())))),
-                                  ElevatedButton(
-                                    child: const Text('Close'),
-                                    onPressed: () => Navigator.pop(context),
-                                  )
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  )));
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Survey test'),
+        ),
+        body: s.SurveyWidget(
+          survey: survey,
+          onChange: (v) {
+            print(v);
+          },
+          onSubmit: (v) {
+            print(v);
+            showModalBottomSheet<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return Container(
+                  height: 400,
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                            child: Container(
+                                child: SingleChildScrollView(
+                                    child: Text(v.toString())))),
+                        ElevatedButton(
+                          child: const Text('Close'),
+                          onPressed: () => Navigator.pop(context),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- example/lib/components/with_controller.dart
+++ example/lib/components/with_controller.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_survey_js/survey.dart' as s;
 
 class WithController extends StatelessWidget {
-  final s.Survey? survey;
+  final s.Survey survey;
   final s.SurveyController surveyController = s.SurveyController();
 
-  WithController({Key? key, this.survey}) : super(key: key);
+  WithController({Key? key, required this.survey}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,58 +14,51 @@ class WithController extends StatelessWidget {
             appBar: AppBar(
               title: Text('Survey test'),
             ),
-            body: survey == null
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : Column(
-                    children: [
-                      Expanded(
-                        child: s.SurveyWidget(
-                          survey: survey!,
-                          onChange: (v) {
-                            print(v);
-                          },
-                          onSubmit: (v) {
-                            print(v);
-                            showModalBottomSheet<void>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return Container(
-                                  height: 400,
-                                  child: Center(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.stretch,
-                                      children: [
-                                        Expanded(
-                                            child: Container(
-                                                child: SingleChildScrollView(
-                                                    child:
-                                                        Text(v.toString())))),
-                                        ElevatedButton(
-                                          child: const Text('Close'),
-                                          onPressed: () =>
-                                              Navigator.pop(context),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                );
-                              },
-                            );
-                          },
-                          hideSubmitButton: true,
-                          controller: surveyController,
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: surveyController.submit,
-                        child: Text(
-                          'Submit',
-                        ),
-                      )
-                    ],
-                  )));
+            body: Column(
+              children: [
+                Expanded(
+                  child: s.SurveyWidget(
+                    survey: survey,
+                    onChange: (v) {
+                      print(v);
+                    },
+                    onSubmit: (v) {
+                      print(v);
+                      showModalBottomSheet<void>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Container(
+                            height: 400,
+                            child: Center(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Expanded(
+                                      child: Container(
+                                          child: SingleChildScrollView(
+                                              child: Text(v.toString())))),
+                                  ElevatedButton(
+                                    child: const Text('Close'),
+                                    onPressed: () => Navigator.pop(context),
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    hideSubmitButton: true,
+                    controller: surveyController,
+                  ),
+                ),
+                TextButton(
+                  onPressed: surveyController.submit,
+                  child: Text(
+                    'Submit',
+                  ),
+                )
+              ],
+            )));
   }
 }
