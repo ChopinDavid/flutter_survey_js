481fdeeaf5e71119787b9aa3a2ba0eda38ef2b10
Dan Couturier <daniel.couturier@wwt.com>
2023-03-15 11:54:36 -0400

    77472 - Added SurveyTitleBuilder tests - DJC

--- test/ui/survey_widget_test.dart
+++ test/ui/survey_widget_test.dart
@@ -1,17 +1,157 @@
 import 'dart:core';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_survey_js/survey.dart';
 import 'package:flutter_survey_js/survey.dart' as s;
 import 'package:flutter_test/flutter_test.dart';
 
 import '../test_data.dart';
 
 main() {
+  group('surveyTitleBuilder', () {
+    testWidgets('is used when title is non-null and Builder is non-null',
+        (widgetTester) async {
+      Key surveyTitleKey = Key('survey-title-builder-key');
+
+      String surveyTitle = 'Some Title';
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: [
+            s.MultiAppLocalizationsDelegate(),
+          ],
+          home: Material(
+            child: s.SurveyWidget(
+              surveyTitleBuilder: (context, survey) {
+                return Container(
+                  key: surveyTitleKey,
+                  child: Text('My New Title'),
+                );
+              },
+              survey: TestData.survey(
+                title: surveyTitle,
+                pages: [
+                  TestData.page(
+                    elements: [
+                      s.Text(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+      await widgetTester.pump();
+      await widgetTester.idle();
+      expect(find.byKey(surveyTitleKey), findsOneWidget);
+      expect(find.text(surveyTitle), findsNothing);
+    });
+
+    testWidgets('is not used when title is null and Builder is non-null',
+        (widgetTester) async {
+      Key surveyTitleKey = Key('survey-title-builder-key');
+      String surveyTitle = 'Some Title';
+
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: [
+            s.MultiAppLocalizationsDelegate(),
+          ],
+          home: Material(
+            child: s.SurveyWidget(
+              surveyTitleBuilder: (context, survey) {
+                return Container(
+                  key: surveyTitleKey,
+                  child: Text('My New Title'),
+                );
+              },
+              survey: TestData.survey(
+                pages: [
+                  TestData.page(
+                    elements: [
+                      s.Text(),
+                    ],
+                  ),
+                ],
+              )..title = null,
+            ),
+          ),
+        ),
+      );
+      await widgetTester.pump();
+      await widgetTester.idle();
+      expect(find.byKey(surveyTitleKey), findsNothing);
+      expect(find.text(surveyTitle), findsNothing);
+    });
+
+    testWidgets('is not used when title is non-null and Builder is null',
+        (widgetTester) async {
+      Key surveyTitleKey = Key('survey-title-builder-key');
+      String surveyTitle = 'Some Title';
+
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: [
+            s.MultiAppLocalizationsDelegate(),
+          ],
+          home: Material(
+            child: s.SurveyWidget(
+              survey: TestData.survey(
+                title: surveyTitle,
+                pages: [
+                  TestData.page(
+                    elements: [
+                      s.Text(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+      await widgetTester.pump();
+      await widgetTester.idle();
+      expect(find.byKey(surveyTitleKey), findsNothing);
+      expect(find.text(surveyTitle), findsOneWidget);
+    });
+
+    testWidgets('is not used when title is null and Builder is null',
+        (widgetTester) async {
+      Key surveyTitleKey = Key('survey-title-builder-key');
+      String surveyTitle = 'Some Title';
+
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          localizationsDelegates: [
+            s.MultiAppLocalizationsDelegate(),
+          ],
+          home: Material(
+            child: s.SurveyWidget(
+              survey: TestData.survey(
+                title: surveyTitle,
+                pages: [
+                  TestData.page(
+                    elements: [
+                      s.Text(),
+                    ],
+                  ),
+                ],
+              )..title = null,
+            ),
+          ),
+        ),
+      );
+      await widgetTester.pump();
+      await widgetTester.idle();
+      expect(find.byKey(surveyTitleKey), findsNothing);
+      expect(find.text(surveyTitle), findsNothing);
+    });
+  });
+
   group('SurveyController', () {
     group('submit', () {
       testWidgets('calls SurveyWidget.onSubmit', (widgetTester) async {
-        final SurveyController controller = SurveyController();
+        final s.SurveyController controller = s.SurveyController();
         int onSubmitCallCount = 0;
         await widgetTester.pumpWidget(
           MaterialApp(
@@ -19,7 +159,7 @@ main() {
               s.MultiAppLocalizationsDelegate(),
             ],
             home: Material(
-              child: SurveyWidget(
+              child: s.SurveyWidget(
                 survey: TestData.survey(
                   pages: [
                     TestData.page(
