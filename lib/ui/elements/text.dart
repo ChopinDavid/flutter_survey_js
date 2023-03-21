8c996fd845fad0fee5586e96d7850888bb7a6e7d
David Chopin <david.chopin@wwt.com>
2023-03-21 15:52:45 -0500

    Revert "77472 - Changed all reactive text fields to use context theme - DJC/DC"
    This reverts commit 3d94d2c01e0b279a341900b638f7e9afa11b09ae.

--- lib/ui/elements/text.dart
+++ lib/ui/elements/text.dart
@@ -13,7 +13,6 @@ final SurveyElementBuilder textBuilder =
   final e = element as s.Text;
   Widget widget = ReactiveTextField(
     formControlName: element.name!,
-    style: Theme.of(context).textTheme.bodyText2,
   );
 
   if (e.inputType == 'date') {
@@ -28,7 +27,6 @@ final SurveyElementBuilder textBuilder =
   if (e.inputType == 'email') {
     widget = ReactiveTextField(
       formControlName: element.name!,
-      style: Theme.of(context).textTheme.bodyText2,
     );
   }
   if (e.inputType == 'datetime') {
@@ -50,7 +48,6 @@ final SurveyElementBuilder textBuilder =
     widget = ReactiveTextField(
       obscureText: true,
       formControlName: element.name!,
-      style: Theme.of(context).textTheme.bodyText2,
     );
   }
   if (e.inputType == 'range') {}
@@ -58,7 +55,6 @@ final SurveyElementBuilder textBuilder =
     widget = ReactiveTextField(
       keyboardType: TextInputType.phone,
       formControlName: element.name!,
-      style: Theme.of(context).textTheme.bodyText2,
     );
   }
   if (e.inputType == 'time') {}
@@ -69,7 +65,6 @@ final SurveyElementBuilder textBuilder =
       keyboardType: TextInputType.number,
       formControlName: element.name!,
       valueAccessor: NumStringValueAccessor(),
-      style: Theme.of(context).textTheme.bodyText2,
     );
   }
   return widget.wrapQuestionTitle(element, hasTitle: hasTitle);
